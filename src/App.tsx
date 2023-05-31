2177f6ecd4e5cd6bf84a798a000b634495048425
Duy Đan <89164062+DuyDan2706@users.noreply.github.com>
2023-05-31 09:32:56 +0700

    dung layout tinh

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
+import Blog from 'pages'
 import React from 'react'
 
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'>
-        <p className='bg-orange-300 text-center'>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-          Learn React
-        </a>
-      </header>
+      <Blog />
     </div>
   )
 }
